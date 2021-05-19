44af94eeb562c40b969fc2c7a42f05a47fc72d1b
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-19 08:09:12 +0530

    Add:Test to check number of entries added

--- src/test/java/com/Indian_Census_Analyzer/CensusAnalyzerTest.java
+++ src/test/java/com/Indian_Census_Analyzer/CensusAnalyzerTest.java
@@ -1,5 +1,16 @@
 package com.Indian_Census_Analyzer;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CensusAnalyzerTest {
+	final String INDIA_CENSUS_CSV_FILE_PATH = "./IndiaCensus.csv";
+
+	@Test
+	public void givenIndianCensusCSVFileReturnsCorrectNumOfRecords() throws IOException {
+		CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
+		int numOfRecords = censusAnalyZer.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+		Assert.assertEquals(1, numOfRecords);
+  }
 
 }
