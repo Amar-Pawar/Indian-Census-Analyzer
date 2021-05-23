105f77f18575b67a7de6cfcdd0445b9438721c95
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-23 17:53:18 +0530

    Add:program to test indian state information from csv file for number if entries added

--- src/main/java/com/Indian_Census_Analyzer/CensusAnalyzer.java
+++ src/main/java/com/Indian_Census_Analyzer/CensusAnalyzer.java
@@ -6,6 +6,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.stream.StreamSupport;
 
 import com.opencsv.CSVReader;
@@ -13,8 +14,9 @@ import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-public class CensusAnalyzer {
-	public int loadIndiaCensusData(String csvFilePath) throws IOException, CensusAnalyzerException {
+public class CensusAnalyzer 
+{
+	public int loadDataFromCSVFile(String csvFilePath) throws IOException, CensusAnalyzerException {
 		try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));)  {
 			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 			csvToBeanBuilder.withType(IndiaCensusCSV.class);
@@ -24,16 +26,15 @@ public class CensusAnalyzer {
 			Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
 			int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
 			return numOfEntries;
-			
 		}catch(IOException e) {
 			throw new CensusAnalyzerException(e.getMessage(),
-					                         CensusAnalyzerException.ExceptionType.CSV_FILE_PROBLEM);
-		}
-		catch(IllegalStateException e) {
-		throw new CensusAnalyzerException(e.getMessage(),
-                							CensusAnalyzerException.ExceptionType.UNABLE_TO_PARSE);
+					                         	CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM);
+		}catch(IllegalStateException e) {
+			throw new CensusAnalyzerException(e.getMessage(),
+                    							CensusAnalyzerException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
+	
 	
 	@SuppressWarnings("deprecation")
 	public boolean loadIndiaCensusDataFile(String csvFilePath) throws FileNotFoundException, IOException, CensusAnalyzerException {
@@ -41,7 +42,7 @@ public class CensusAnalyzer {
 		try{
 			reader= new CSVReader(new FileReader(csvFilePath),'.');
 			String [] nextLine;
-			while ((nextLine=reader.readNext())!= null)   
+			while ((nextLine=reader.readNext())!= null)  //returns a boolean value  
 			{  
 				   return true;   
 			}   
--- src/test/java/com/Indian_Census_Analyzer/CensusAnalyzerTest.java
+++ src/test/java/com/Indian_Census_Analyzer/CensusAnalyzerTest.java
@@ -4,50 +4,65 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class CensusAnalyzerTest {
-	final String INDIA_CENSUS_CSV_FILE_PATH = "./IndiaCensus.csv";
-	private static final String WRONG_CSV_FILE_PATH = "IndiaCensus.csv";
-	private static final String WRONG_FILE_TYPE = "./IndiaCensus.pdf";
-
+public class CensusAnalyzerTest 
+{
+	private final String INDIAN_CENSUS_CSV_FILE_PATH = "./IndiaCensus.csv";
+	private static final String WRONG_CENSUS_CSV_FILE_PATH = "IndiaCensus.csv";
+	private static final String WRONG_CENSUS_FILE_TYPE_PATH = "./IndiaCensus.pdf";
+	
+	private final String INDIAN_STATE_CODE_CSV_FILE_PATH = "./StateCodes.csv";
+	
+	//test for census analysis
 	@Test
 	public void givenIndianCensusCSVFileReturnsCorrectNumOfRecords() throws IOException, CensusAnalyzerException {
 		CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
-		int numOfRecords = censusAnalyZer.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+		int numOfRecords = censusAnalyZer.loadDataFromCSVFile(INDIAN_CENSUS_CSV_FILE_PATH);
 		Assert.assertEquals(1, numOfRecords);
-  }
+	}
+	
 	@Test
-	public void given_IndiaCensusData_With_Wrong_File_Should_Throw_Exception() throws IOException {
+	public void given_IndiaCensusData_WithWrongFile_ShoulThrewException() throws IOException {
 		try {
 			CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(CensusAnalyzerException.class);
-			censusAnalyZer.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+			censusAnalyZer.loadDataFromCSVFile(WRONG_CENSUS_CSV_FILE_PATH);
 		} catch(CensusAnalyzerException e) {
 			Assert.assertEquals(CensusAnalyzerException.ExceptionType.CSV_FILE_PROBLEM, e.type);
 		}
 	}
+	
 	@Test
-	public void given_IndiaCensusData_With_Correct_File_ButWrongType_Should_Throw_Exception() throws IOException{
+	public void given_IndiaCensusData_WithCorrectFile_ButWrongType_ShoulThrewException() throws IOException{
 		try {
 			CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(IOException.class);
-			censusAnalyZer.loadIndiaCensusData(WRONG_FILE_TYPE);
+			censusAnalyZer.loadDataFromCSVFile(WRONG_CENSUS_FILE_TYPE_PATH);
 		} catch(CensusAnalyzerException e) {
 			Assert.assertEquals(CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
 		}
 	}
 	
 	@Test
-	public void given_IndiaCensusData_WithCorrect_File_But_Wrong_Delimiter_Should_Throw_Exception() throws IOException{
+	public void given_IndiaCensusData_WithCorrectFile_ButWrongDelimiter_ShoulThrewException() throws IOException{
 		try {
 			CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(IOException.class);
-			censusAnalyZer.loadIndiaCensusDataFile(INDIA_CENSUS_CSV_FILE_PATH);
+			censusAnalyZer.loadIndiaCensusDataFile(INDIAN_CENSUS_CSV_FILE_PATH);
 		} catch(CensusAnalyzerException e) {
 			Assert.assertEquals(CensusAnalyzerException.ExceptionType.UNABLE_TO_PARSE, e.type);
 		}
 	}
-
+	
+	
+	//test for state code information
+	@Test
+	public void givenStateCodeCSVFileReturnsCorrectNumOfRecords() throws IOException, CensusAnalyzerException {
+		CensusAnalyzer statecodeAnalyZer = new CensusAnalyzer();
+		int numOfRecords = statecodeAnalyZer.loadDataFromCSVFile(INDIAN_STATE_CODE_CSV_FILE_PATH);
+		Assert.assertEquals(1, numOfRecords);
+	}
+		
 }
